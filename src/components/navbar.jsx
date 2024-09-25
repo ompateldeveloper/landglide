55a578f6823f737ed6934344fb9797c24b7ac1f0
ompateldeveloper <ompateldeveloper@gmail.com>
2024-09-25 18:40:51 +0530

    strict false

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,7 +1,6 @@
 "use client";
 import React from "react";
 import { Button } from "./ui/button";
-import { useRouter } from "next/navigation";
 import Link from "next/link";
 import { Avatar, AvatarFallback, AvatarImage } from "./ui/avatar";
 import { Sheet, SheetClose, SheetContent, SheetDescription, SheetFooter, SheetHeader, SheetTitle, SheetTrigger } from "@/components/ui/sheet";
--- src/middleware.ts
+++ src/middleware.ts
@@ -1,6 +1,7 @@
 import type { NextRequest } from "next/server";
 
 export function middleware(request: NextRequest) {
+    
     // const token=request.cookies.get("token");
     // const admin = false;
     // const url = request.nextUrl.clone();
